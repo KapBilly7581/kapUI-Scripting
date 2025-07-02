3bec249e59bee558cf10b165bd63e9f27b57674e
KapBilly7581 <pusaarda0634@gmail.com>
2025-07-02 19:06:51 +0300

    Add files via upload

---
+++ src/types/enums/AnimTypes.ts
@@ -0,0 +1,11 @@
+export enum AnimationTypes {
+	Alpha = "alpha",
+	Clip = "clip",
+	Color = "color",
+	FlipBook = "flip_book",
+	Offset = "offset",
+	Size = "size",
+	Uv = "uv",
+	Wait = "wait",
+	AsepriteFlipBook = "aseprite_flip_book",
+}
